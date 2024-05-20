04ce2792c8b02bda7227f5a7f2388d7e509b6e6a
NadiahFitri <nadiahfitrihidayati1@gmail.com>
2024-05-20 07:43:59 +0700

    maps.php

--- app/user/maps.php
+++ app/user/maps.php
@@ -154,6 +154,75 @@
                 }
             }
 
+            .container {
+                display: flex;
+                justify-content: space-between;
+                padding: 20px;
+                background-color: #fff;
+            }
+            .section {
+                display: flex;
+                flex-direction: column;
+                width: 20%;
+            }
+            .section1 {
+                display: flex;
+                flex-direction: column;
+                width: 40%;
+                margin-left:700px;
+                
+            }
+            .section h2 {
+                margin: 0;
+                margin-bottom: 10px;
+                font-size: 24px;
+                text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
+            }
+            .section1 h2 {
+                margin: 0;
+                margin-bottom: 10px;
+                font-size: 24px;
+                text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
+            
+            }
+            .links {
+                display: flex;
+                gap: 20px; 
+            }
+            .links a {
+                text-decoration: none;
+                color: #000;
+                font-weight: bold;
+                text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
+            }
+            .social-icons {
+                display: flex;
+                gap: 15px;
+                align-items: center;
+            }
+            .social-icons a {
+                text-decoration: none;
+                color: #000;
+                font-size: 24px;
+            }
+            .section hr {
+                border: 0;
+                height: 3px; 
+                background: #000; 
+                margin-top: 1px;
+                margin-bottom:15px;
+                width: 100%;
+            }
+            .section1 hr {
+                border: 0;
+                height: 3px; 
+                background: #000; 
+                margin-top: 1px;
+                margin-bottom:15px;
+                width: 90%; 
+                margin-left:0;
+            }
+
             .footer {
                 background-color: #333;
                 color: #fff;
@@ -221,6 +290,28 @@
                     <a href="https://wa.me/+62 878-5305-3661" target="_blank">
                         <img src="../../asset/img/logo_WA.png" alt="WhatsApp" class="whatsapp-logo">
                     </a>
+                </div>
+
+                <div class="container">
+                    <div class="section1">
+                        <h2>About Us</h2>
+                        <hr>
+                        <div class="links">
+                            <a href="profil_kami.php">PROFIL KAMI</a>
+                            <a href="t&c.php">T & C</a>
+                            <a href="formulirbooking.php">BOOKING TICKET</a>
+                        </div>
+                    </div>
+                    <div class="section">
+                        <h2>Follow Us</h2>
+                        <hr>
+                        <div class="social-icons">
+                            <a href="https://www.facebook.com/login.php"><img src="../../asset/img/fc.png" alt="Facebook"></a>
+                            <a href="https://x.com/"><img src="../../asset/img/tweet.png" alt="Twitter"></a>
+                            <a href="https://www.instagram.com/"><img src="../../asset/img/ig.png" alt="Instagram"></a>
+                            <a href="https://www.youtube.com/"><img src="../../asset/img/youtube.png" alt="YouTube"></a>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="footer" id="footer">
