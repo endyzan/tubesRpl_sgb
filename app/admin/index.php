32880acefd7d4e4d830137ffa24c67936b520028
endyzan <andizainuri12@gmail.com>
2024-05-21 10:59:57 +0700

    direct langsung ke user

--- app/admin/index.php
+++ app/admin/index.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login Admin</title>
     <link rel="stylesheet" href="../../asset/css/admin.css" type="text/css">
 </head>
 <body>
