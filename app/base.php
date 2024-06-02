58df845cc99823021e116286f9bae2a97e86e09f
blackhair4254 <betadata03@gmail.com>
2024-06-02 14:34:36 +0700

    Update base.php

--- app/base.php
+++ app/base.php
@@ -1,11 +1,16 @@
 <?php
-$host = 'localhost';
-$db = 'rplsgb';
-$user = 'root';
+$host = "localhost";
+$dbname = "wisatastadionbangkalan";
+$dbnameAndi = "rplsgb";
+$user = "root";
 $pass = '';
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $dbnameAndi);
+
+$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+?>
