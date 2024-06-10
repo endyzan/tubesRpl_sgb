77b09acf8878ccf380253fa57d62ad8183a3ece0
NadiahFitri <nadiahfitrihidayati1@gmail.com>
2024-06-10 21:02:54 +0700

    formulirbooking.php

--- app/user/formulirbooking.php
+++ app/user/formulirbooking.php
@@ -5,210 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulir Booking</title>
     <link rel="stylesheet" href="../../asset/css/styles.css"> <!-- Link ke file CSS eksternal -->
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            outline: none;
-            border: none;
-            text-decoration: none;
-        }
-        html {
-            scroll-behavior: smooth;
-        }
-        body {
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        }
-
-        /* ---------------------------------Header Start--------------------------------- */
-        .header {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            background: white;
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);
-        }
-        .logo {
-            display: flex;
-            width: 120px;
-            height: 120px;
-        }
-        /* ---------------------------------Header End--------------------------------- */
-        
-        /* ---------------------------------Menu Start--------------------------------- */
-        .menu ul {
-            list-style: none;
-        }
-        .menu ul li {
-            position: relative;
-            float: left;
-        }
-        .menu ul li a {
-            color: black;
-            font-size: 1.3rem;
-            font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-            display: block;
-            padding: 27px;
-            font-weight: bold;
-        }
-        .menu ul li ul {
-            position: absolute;
-            background: #D3D3D3;
-            display: none;
-            width: max-content;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);
-        }
-
-        .menu ul li ul li {
-            border-top: 1px solid rgba(207, 205, 205, 0.384);
-            position: left;
-        }
-
-        .menu ul li ul li a{
-            font-size: 0.8rem;
-            padding: 10px 20px;
-            position: left;
-        }
-        .menu ul li ul li:not(:last-child)::after {
-            content: "";
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: 1px;
-            background: black;
-        }
-        .menu ul li:hover > ul {
-            display: flex;
-            flex-direction: column;
-        }
-        .menu a:hover {
-            color: red;
-        }
-        .menu a::after {
-            content: "";
-            border-bottom: 0.1rem solid black;
-            display: block;
-            padding-bottom: 0.5rem;
-            transform: scaleX(0);
-        }
-        .menu a:hover::after {
-            transform: scaleX(1.0);
-            transition: 0.2s linear;
-        }
-
-        .pembuka img {
-            width: 100%;
-            margin-top: 170px;
-        }
-        /* ---------------------------------Menu End--------------------------------- */
-
-        .sticky-whatsapp2 {
-                position: fixed;
-                bottom: 60px;
-                right: 0px;
-                z-index: 100;
-            }
-
-            .sticky-whatsapp2 img {
-                width: 80px;
-                height: 50px;
-            }
-
-            @media (max-width: 768px) {
-                .sticky-whatsapp2 img {
-                    width: 20px;
-                    height: 20px;
-                }
-            }
-
-            .container2 {
-                display: flex;
-                justify-content: space-between;
-                padding: 20px;
-                background-color: #fff;
-            }
-            .section {
-                display: flex;
-                flex-direction: column;
-                width: 20%;
-            }
-            .section1 {
-                display: flex;
-                flex-direction: column;
-                width: 40%;
-                margin-left:700px;
-                
-            }
-            .section h2 {
-                margin: 0;
-                margin-bottom: 10px;
-                font-size: 24px;
-                text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
-            }
-            .section1 h2 {
-                margin: 0;
-                margin-bottom: 10px;
-                font-size: 24px;
-                text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
-            
-            }
-            .links {
-                display: flex;
-                gap: 20px; 
-            }
-            .links a {
-                text-decoration: none;
-                color: #000;
-                font-weight: bold;
-                text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
-            }
-            .social-icons {
-                display: flex;
-                gap: 15px;
-                align-items: center;
-            }
-            .social-icons a {
-                text-decoration: none;
-                color: #000;
-                font-size: 24px;
-            }
-            .section hr {
-                border: 0;
-                height: 3px; 
-                background: #000; 
-                margin-top: 1px;
-                margin-bottom:15px;
-                width: 100%;
-            }
-            .section1 hr {
-                border: 0;
-                height: 3px; 
-                background: #000; 
-                margin-top: 1px;
-                margin-bottom:15px;
-                width: 90%; 
-                margin-left:0;
-            }
-
-            .footer2 {
-                background-color: #333;
-                color: #fff;
-                padding: 10px;
-                text-align: center;
-                margin-top: 80px;
-            }
-
-            .footer2 p {
-                margin: 0;
-            }
-    </style>
 </head>
 <body>
     <!-- Header Start -->
@@ -239,12 +35,12 @@
     </div>
     <!-- Header Start -->
 
-    <div class = 'isi'>
+    <div class = "isi">
         <div class="curved-box-title">ONLINE TICKET BOOKING</div>
         <div class="orangetext">Mau Download Ticket?</div>
         <a href="cektiketmasuk.php">Download disini</a>
-        <div class="orangetext">Sudah Punya Tiket Masuk?</div>
-        <a href="cektiketmasuk.php">Download disini</a>
+        <div class="orangetext">MAU TAMBAH PERMAINAN ?</div>
+        <a href="cektiketmasuk.php">Klik disini</a>
         <!-- <div class='sekat'></div> -->
 
         <form id="booking-form" action="pilihpembayaran.php" method="POST">
@@ -315,7 +111,7 @@
                     <div class="grid-item"><label for="nama">Nama*</label></div>
                     <div class="grid-item"><label for="email"></label></div>
 
-                    <div class="grid-item"><input type="text" id="nama" name="nama" required></div>
+                    <div class="grid-item"><input type="text" id="nama" name="nama" required pattern="[A-Za-z]+"></div>
                 </div>
 
                 <div class="subgrid">
@@ -347,7 +143,7 @@
     </div>
 
     <div class="sticky-whatsapp2">
-        <a href="https://wa.me/+62 878-5305-3661" target="_blank">
+        <a href="https://wa.me/+6287853053661" target="_blank">
             <img src="../../asset/img/logo_WA.png" alt="WhatsApp" class="whatsapp-logo">
         </a>
     </div>
@@ -474,7 +270,7 @@
                         <div class="text-box">
                             <div class="text-item">${jenis}</div>
                             <div class="text-item">${harga}</div>
-                            <div class="text-item">${durasi} menit</div>
+                            <div class="text-item">${durasi}</div>
                             <button type="button" class="btn-minus-permainan" data-id="${id}" data-harga="${harga}">-</button>
                             <div class="text-item"><input type="number" name="${id}" id="${id}" value="1" min="1" readonly></div>
                             <button type="button" class="btn-plus-permainan" data-id="${id}" data-harga="${harga}">+</button>
